53e5591efce6650e7d826d9478c39ae741a81176
krizcom <rohankarkera14@gmail.com>
2021-08-02 13:39:25 +0530

    log1.1

--- loginapp/models.py
+++ loginapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class employee(models.Model):
+    idlogin =  models.IntegerField()
+    empid = models.IntegerField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    authrank = models.CharField(max_length=1)
+
